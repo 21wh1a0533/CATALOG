f083d4d0693271414fab1fbe41b9df9a6ff8c405
Dyapa Sai Akshara <137030356+21wh1a0533@users.noreply.github.com>
2025-01-02 12:40:35 +0530

    Update and rename Shamir's Secret Sharing.java to Shamirs Secret Sharing.java

--- Shamirs Secret Sharing.java
+++ Shamirs Secret Sharing.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SS {
+public class ShamirsSecretSharing {
     public static void main(String[] args) {
         try {
             
